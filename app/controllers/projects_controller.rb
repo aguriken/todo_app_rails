ba63763fc38d0625234efd332becbd30d2c5ea4c
中原賢一 <ohagurokenichi@yahoo.co.jp>
2020-02-22 18:16:09 +0900

    Create simple view for all pages.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,23 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      flash[:success] = 'プロジェクトの作成に成功しました。'
+      redirect_to "/projects/#{project.id}/tasks"
+    else
+      flash.now[:danger] = 'プロジェクトの作成に失敗しました'
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
 end
